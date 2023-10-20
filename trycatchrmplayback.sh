226080a747d038ab8a1302bdcd7c54b8498c25ac
Ong Wen Hui <111413221+Ongwenhui@users.noreply.github.com>
2023-10-20 16:26:14 +0800

    Create trycatchrmplayback.sh

---
+++ trycatchrmplayback.sh
@@ -0,0 +1,25 @@
+function try()
+{
+    [[ $- = *e* ]]; SAVED_OPT_E=$?
+    set +e
+}
+
+function throw()
+{
+    exit $1
+}
+
+function catch()
+{
+    export exception_code=$?
+    (( $SAVED_OPT_E )) && set +e
+    return $exception_code
+}
+
+try
+(
+    docker rm -f mqttpunggol
+)
+catch || {
+    echo "no mqttpunggol moving on"
+}
